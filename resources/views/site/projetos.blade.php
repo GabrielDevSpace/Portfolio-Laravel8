aeaf5b6be1e5bb4d14573452f3db2b135ed23c71
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2022-04-11 21:18:27 -0300

    Pagina Projetos Criada

--- resources/views/site/projetos.blade.php
+++ resources/views/site/projetos.blade.php
@@ -19,11 +19,41 @@
                     </div>
                 </div>
             </nav>
-            <div align='center'>
-                <div align='center'>
-                    <h4 class="texto-dark"><i class="em em-building_construction" aria-role="presentation" aria-label=""></i> <{ Em Desenvolvimento. }> </h4>
+            <div class="container-fluid">
+                <div class="row py-4">
+                    
+                    <div class="col-md-3 py-2 link-projetos">
+                    <a target="_BLANK" href="{{'/coinapi'}}">
+                    <h6 class="texto-dark"><i class="fa fa-btc texto-gold"></i> CoinAPI</h6>
+                    <small class="texto-secondary">
+                        API para listar a cotação das 1000 principais cripto-moedas do mercado e API secundaria para obter a taxa de câmbio USD | BRL.
+                    </small>                    
+                    </a>
+                    </div>
+
+                    <div class="col-md-3 py-2 link-projetos">
+                    <i class="fa fa-lock login-fa" title="Login Exigido!"></i>
+                    <a target="_BLANK" href="https://developerspace.com.br/projetos_antigos/kanban">
+                    <h6 class="texto-dark"><i class="fa fa-tasks text-warning"></i> Tasks</h6>
+                    <small class="texto-secondary">
+                    Tasks é um projeto privado para controlar minhas demandas como gestor e ter uma visão geral das atividades da minha equipe.
+                    </small>                    
+                    </a>
+                    </div>
+
+                    <div class="col-md-3 py-2 link-projetos">
+                    <i class="fa fa-lock login-fa" title="Login Exigido!"></i>
+                    <a target="_BLANK" href="https://developerspace.com.br/projetos_antigos/webtv">
+                    <h6 class="texto-dark"><i class="fa fa-television text-primary"></i> WebTv </h6>
+                    <small class="texto-secondary">
+                    Player web de listas IPTV, realiza o tratamento e exibição dos Canais & VOD's, eliminando a necessidade de apps especificos.
+                    </small>                    
+                    </a>
+                    </div>
+
+                    <div class="line"></div>
+
                 </div>
-                
             </div>
            
 @endsection
